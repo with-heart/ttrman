61ca38b48e5b944938fd2f0837a6242b526946a7
with-heart <with.heart+git@pm.me>
2024-12-22 17:57:52 -0500

    initialize repo

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ttrman</title>
+        <meta
+          name="description"
+          content="Open source game manager for the Ticket To Ride board game"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <div>ttrman</div>
+      </main>
+    </>
+  )
+}
